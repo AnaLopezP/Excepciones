4808aec65565cf038966c6f8e10dba9285c85d0b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-31 18:40:29 +0200

    funciones en clase

--- Excepciones/excepciones.py
+++ Excepciones/excepciones.py
@@ -1,10 +1,27 @@
-'''class web:
+from errno import ESTALE
+
+
+class web:
     def __init__(self, correo):
         self.correo = correo
     def get_correo(self):
         return self.correo
     def set_correo(self, correo):
-        self.correo = correo'''
+        self.correo = correo
+
+    def esta_arroba(correo):
+        arroba = "@"
+        if arroba in correo:
+            return True
+        else:
+            return False
+
+    def esta_punto(correo):
+        punto = "."
+        if punto in correo:
+            return True
+        else:
+            return False
 
 
 def introduzca_correo(contador):
@@ -15,23 +32,15 @@ def introduzca_correo(contador):
     correcto2 = "."
     print(correcto)
     print(correcto2)
-    if correcto not in correo:
+    if web.esta_arroba == True and web.esta_punto == True:
+        print("bienvenido")
+    else:
         print("no es valido. Le quedan " + str(contador - 1) + " intentos")
         if contador != 1:
             contador = contador - 1
             introduzca_correo(contador)
         else:
             print("La sesion ha sido bloqueada por demasiados intentos")
-
-    elif correcto2 not in correo:
-        print("no es valido. Le quedan " + str(contador - 1) + " intentos")
-        if contador != 1:
-            contador = contador - 1
-            introduzca_correo(contador)
-        else:
-            print("La sesion ha sido bloqueada por demasiados intentos")  
-    else:
-        print("bienvenido")
 
 contador = 3
 introduzca_correo(contador)
