78052fad1886262e104f0c2871678be1e3ef9bcb
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-31 09:02:13 +0200

    añadir correcto2

--- Excepciones/excepciones.py
+++ Excepciones/excepciones.py
@@ -9,19 +9,20 @@ import re
         self.correo = correo'''
 
 
-
 def introduzca_correo(contador):
     print(contador)
     print("Introduzca la direccion de correo electronico")
     correo = input()
-    correcto = re.search("@ ", correo)
+    correcto = re.search("@", correo)
+    correcto2 = re.search(".", correo)
     if correcto == None:
-        print("no es valido. Le quedan " + str(contador - 1) + " intentos")
-        if contador != 0:
-            contador = contador - 1
-            introduzca_correo(contador)
-        else:
-            print("La sesion ha sido bloqueada por demasiados intentos")
+        if correcto2 == None
+            print("no es valido. Le quedan " + str(contador - 1) + " intentos")
+            if contador != 0:
+                contador = contador - 1
+                introduzca_correo(contador)
+            else:
+                print("La sesion ha sido bloqueada por demasiados intentos")
         
        
     else:
